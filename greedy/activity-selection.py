254748c12747c5d6db4fc74d25cec253d24d886c
Khalid Hamza <khalid_hamza2015@yahoo.com>
2023-08-19 21:38:55 +0300

    add comments

--- greedy/activity-selection.py
+++ greedy/activity-selection.py
@@ -1,4 +1,7 @@
 # Activity Selection Algorithm
+# start and end refer to the start time and end time for a lecture.
+# We have to get all lectures which we can attend
+# the logic is to select the lecture if the end time is less than or equal to the start time of the next lecture
 
 start   = [9, 10, 11, 12, 13, 15]
 end     = [11, 11, 12, 14, 15, 16]
---
+++ greedy/characters-frequencies.py
@@ -0,0 +1,11 @@
+# characters frequency
+# get the frequency of each character
+
+word    = "Hello World";
+result  = {}
+for char in word:
+    count = result.get(char, 0);
+    count += 1;
+    result[char] = count
+
+print(result)
